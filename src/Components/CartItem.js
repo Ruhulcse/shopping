9d437c6c1de48b8cf2e71a1a11b83b91f3fbdb0b
Ruhulcse <Ruhul.cse7862@gmail.com>
2021-07-14 22:02:08 +0600

    finalize

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -2,15 +2,15 @@ import { useContext } from "react";
 import "./CartItem.css";
 import CartContext from "../context/cart/CartContext";
 import formatCurrency from "format-currency";
-
+import pic from "../image/dummy.png"
 const CartItem = ({ item }) => {
   const { removeItem } = useContext(CartContext);
   let opts = { format: "%s%v", symbol: "€" };
   return (
     <li className='CartItem__item'>
-      <img src={item.image} alt='' />
+      <img src={pic} alt='' />
       <div>
-        {item.name} {formatCurrency(`${item.price}`, opts)}
+        {item.productName} {formatCurrency(`${item.price}`, opts)}
       </div>
       <button className='CartItem__button' onClick={() => removeItem(item._id)}>
         Remove
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,27 @@
-import products from "../data";
+import React,{useEffect, useState} from 'react'
+import {URL} from "../utils/config"
+import axios from 'axios'
 import "./HomScreen.css";
 import ProductCard from "../Components/ProductCard";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    try {
+      async function fetchUserData() {
+        const {data} = await axios.get(`${URL}products`);
+        setProducts(data);
+        console.log(data);
+      }
+      fetchUserData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return (
     <div className='products__wrapper'>
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <ProductCard key={product._id} product={product} />
       ))}
     </div>
   );
